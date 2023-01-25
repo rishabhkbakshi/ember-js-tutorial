cdd9969c58c25dfa32a183c8e93bb9dfd5370f93
Rishabh Bakshi <rishabh.bakshi@clarivate.com>
2023-01-25 17:47:38 +0530

    understand the randering/integration testing

--- tests/integration/components/product/details-test.js
+++ tests/integration/components/product/details-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-js-tutorial/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | product/details', function (hooks) {
@@ -10,17 +10,41 @@ module('Integration | Component | product/details', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Product::Details />`);
+    this.set('price', {
+      original: 50,
+      current: 30,
+    });
 
-    assert.dom(this.element).hasText('');
+    this.set('onChangeColor', function (color) {
+      assert.equal(color, 'red');
+    });
+    this.set('colors', [{ color: 'red' }]);
 
-    // Template block usage:
-    await render(hbs`
-      <Product::Details>
-        template block text
-      </Product::Details>
-    `);
+    await render(hbs`<Product::Details 
+      @price={{this.price}} 
+      @colors={{this.colors}}
+      @onChangeColor={{this.onChangeColor}}
+      @isDetails={{true}}
+    />`);
 
-    assert.dom(this.element).hasText('template block text');
+    // await this.pauseTest();
+
+    assert.dom('[data-test-original-price]').hasText('$50.00');
+    assert.dom('[data-test-current-price]').hasText('$30.00');
+
+    await click('[data-test-color]');
+
+    // await this.pauseTest();
+
+    // assert.dom(this.element).hasText('');
+
+    // // Template block usage:
+    // await render(hbs`
+    //   <Product::Details>
+    //     template block text
+    //   </Product::Details>
+    // `);
+
+    // assert.dom(this.element).hasText('template block text');
   });
 });
